56d49777d93a601c3e9e581adf8aa4e669f85061
namem2908 <nguyenphuongnam29082003@gmail.com>
2025-05-09 16:58:59 +0700

    login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Dashboard from './pages/Dashboard';
 import Home from './pages/Home';
+import Login from './pages/Login';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/dashboard" element={<Dashboard />} />  
           <Route path="/home" element={<Home />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </div>
     </Router>
